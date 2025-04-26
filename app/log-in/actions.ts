74c06e7bf8466fad82594959553bad183868eda1
SeulBin Lee <96245157+charmleesb@users.noreply.github.com>
2025-04-26 12:25:09 +0900

    refactor: log-in 페이지 유효성 코드 수정

--- app/log-in/actions.ts
+++ app/log-in/actions.ts
@@ -1,23 +1,29 @@
 "use server";
+
 import {z} from "zod";
+import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "../lib/constants";
 
 const checkEmail = (email:string) => email.endsWith("@zod.com");
-const checkPassword = (password:string) => /\d/.test(password);
 
 const formSchema = z.object({
   email: z.string().email().refine(checkEmail, "Only @zod.com emails are allowed"),
-  username: z.string().min(5, "Username should be at least 5 characters long."),
-  password: z.string().min(10, "Password should be at least 10 characters long.").refine(checkPassword, "Password should contain at least one number (0123456789).")
+  username: z.string(),
+  password: z.string().min(PASSWORD_MIN_LENGTH).regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR)
 })
 
-export async function handleForm(prevState: any, formData: FormData) {
+export async function login(prevState: any, formData: FormData) {
   const data = {
-    email: formData.get("email"),
-    username: formData.get("username"),
-    password: formData.get("password")
+    email: formData.get("email") as string,
+    username: formData.get("username" as string),
+    password: formData.get("password" as string)
   }
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
+  
   if (!result.success) {
-    return result.error.flatten();
-  } 
+    console.log(result.error.flatten());
+    return { fieldErrors: result.error.flatten().fieldErrors };
+  } else {
+    console.log(result.data);
+    return { success: true };
+  }
 }
