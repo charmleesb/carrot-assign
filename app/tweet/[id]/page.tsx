e68441e4c0515cf10c2ae4ee6ec2b8096172bd34
SeulBin Lee <96245157+charmleesb@users.noreply.github.com>
2025-04-29 00:34:19 +0900

    feat: bio 추가

--- app/tweet/[id]/page.tsx
+++ app/tweet/[id]/page.tsx
@@ -30,7 +30,12 @@ export default async function TweetDetail({params,}:{params: {id:string}}) {
   }
   return (
     <div className="flex flex-col bg-sky-100 h-screen p-5">
-      <span className="text-xl border-b border-neutral-400 pb-2">{tweet.user.username}</span>
+      <div className="flex justify-between items-center border-b border-neutral-400 pb-2">
+        <span className="text-xl">{tweet.user.username}</span>
+        <div className="w-5 h-5 bg-sky-800 rounded-full flex items-center justify-center">
+          <span className="text-white text-sm">{tweet.user.bio}</span>
+        </div>
+      </div>
       <span className="py-10">{tweet.tweet}</span>
       <span className="text-right text-sm text-neutral-500">{formatToTimeAgo(tweet.created_at.toString())}</span>
     </div>
