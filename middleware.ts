a509f41eca4f9b04171bd1f7b70812d122241d27
SeulBin Lee <96245157+charmleesb@users.noreply.github.com>
2025-05-12 22:27:58 +0900

    fix: log-in 페이지 안가는 부분 수정

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 // middleware.ts
 import { NextRequest, NextResponse } from "next/server"
+import getSession from "./lib/session"
 
 interface Routes {
   [key: string]: boolean
@@ -12,8 +13,12 @@ const publicOnlyUrls: Routes = {
 
 export function middleware(request: NextRequest) {
   const cookie = request.cookies.get("delicious-carrot")?.value
-  const isPublicPage = publicOnlyUrls[request.nextUrl.pathname]
-
+  const isPublicPage = publicOnlyUrls[request.nextUrl.pathname];
+  
+  if (!cookie && !isPublicPage) {
+    // 로그인 안 했고 보호된 경로면 → 로그인 페이지로 리다이렉트
+    return NextResponse.redirect(new URL("/log-in", request.url));
+  }
   // 로그인된 유저가 public 페이지에 접근 시 홈으로 리디렉션
   if (cookie && isPublicPage) {
     return NextResponse.redirect(new URL("/", request.url))
